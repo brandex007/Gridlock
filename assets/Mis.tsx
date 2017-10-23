0f32345fb42d771a9e11e2ce441ec3c69bd0bf2d
aagiu2 <32198077+aagiu2@users.noreply.github.com>
2017-10-23 16:34:56 -0400

    updated files for map

---
+++ assets/Mis.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="4tilesets" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="4tilesets.png" width="1024" height="1024"/>
+</tileset>
