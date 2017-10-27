9cb06abd8b9ec9916030b184fdb416a673541625
aagiu2 <32198077+aagiu2@users.noreply.github.com>
2017-10-26 20:04:35 -0400

    update assets and map

---
+++ core/assets/items2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="items" tilewidth="32" tileheight="32" tilecount="434" columns="14">
+ <image source="ed8753041bee747ccb27029e8843dfa7--game-icon-free-games.jpg" width="476" height="1020"/>
+</tileset>
