b10733d63d680d0fc3cbae5511b92798f3ebf463
aagiu2 <32198077+aagiu2@users.noreply.github.com>
2017-11-16 09:42:14 -0500

    update trace ability numbers

--- core/src/com/innovativeexecutables/Score.java
+++ core/src/com/innovativeexecutables/Score.java
@@ -3,11 +3,11 @@ package com.innovativeexecutables;
 public class Score {
     private float scoreCounter = 0;
     
-    // 1.2.2. Score must start at 200
+    // 3.1.2.2. Score must start at 200
     private int score = 200;
     
     public Score(){
-        // 1.2.1. Score must subtract by 2 every 30 seconds
+        // 3.1.2.1. Score must subtract by 2 every 30 seconds
         com.badlogic.gdx.utils.Timer.schedule(new com.badlogic.gdx.utils.Timer.Task(){
             @Override
             public void run() {
