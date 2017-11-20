a4ab753f694ead6e2f615274944c73b3fd5d9fe4
aagiu2 <32198077+aagiu2@users.noreply.github.com>
2017-11-20 15:08:20 -0500

    Delete stairs2.tsx

--- stairs2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="stairs2" tilewidth="32" tileheight="32" tilecount="121" columns="11">
- <image source="../../../../../Mis/Game Art/Sewer_level_example.png" width="352" height="352"/>
-</tileset>
