8827a6fc6e10140745f9951c739261df97372b2f
aagiu2 <32198077+aagiu2@users.noreply.github.com>
2017-11-22 18:45:56 -0500

    Map 2 mods

--- core/assets/dungeon1.tsx
+++ core/assets/dungeon1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="dungeon1" tilewidth="32" tileheight="32" tilecount="512" columns="32">
- <image source="dungeon1.png" width="1024" height="512"/>
+<tileset name="dungeon_items" tilewidth="32" tileheight="32" tilecount="476" columns="14">
+ <image source="dungeon_items.png" width="448" height="1088"/>
 </tileset>
